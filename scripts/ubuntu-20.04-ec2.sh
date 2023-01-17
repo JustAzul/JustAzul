7b2cd5557d9e0a15646095aef4e875acf87cda3f
Azul <metalloger@gmail.com>
2023-01-17 14:14:22 -0300

    updated curl address

--- scripts/ubuntu-20.04-ec2.sh
+++ scripts/ubuntu-20.04-ec2.sh
@@ -3,4 +3,4 @@ sudo apt-get update
 sudo apt-get -y upgrade
 sudo ln -fs /usr/share/zoneinfo/America/Recife /etc/localtime 
 sudo dpkg-reconfigure -f noninteractive tzdata
-curl https://raw.githubusercontent.com/JustAzul/JustAzul/main/swap.sh | bash
+curl https://raw.githubusercontent.com/JustAzul/JustAzul/main/scripts/swap.sh | bash
