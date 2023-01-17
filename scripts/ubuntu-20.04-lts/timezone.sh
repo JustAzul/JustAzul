c016ac0b27bb09a21e8afd5702c946cec30be2d1
Azul <metalloger@gmail.com>
2023-01-17 16:22:24 -0300

    added option to select timezone

--- scripts/ubuntu-20.04-lts/timezone.sh
+++ scripts/ubuntu-20.04-lts/timezone.sh
@@ -1,4 +1,34 @@
 #!/bin/bash
 
-# Set the timezone to America/Recife
-sudo ln -fs /usr/share/zoneinfo/America/Recife /etc/localtime && sudo dpkg-reconfigure -f noninteractive tzdata
+current_timezone=$(date +%Z)
+echo "Current timezone: $current_timezone"
+
+PS3='Please select your timezone: '
+options=("America/Recife" "America/New_York" "Asia/Tokyo" "Europe/Berlin" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "America/Recife")
+            timezone="America/Recife"
+            break
+            ;;
+        "America/New_York")
+            timezone="America/New_York"
+            break
+            ;;
+        "Asia/Tokyo")
+            timezone="Asia/Tokyo"
+            break
+            ;;
+        "Europe/Berlin")
+            timezone="Europe/Berlin"
+            break
+            ;;
+        "Quit")
+            exit 0
+            ;;
+        *) echo "Invalid option $REPLY";;
+    esac
+done
+
+sudo ln -fs /usr/share/zoneinfo/$timezone /etc/localtime && sudo dpkg-reconfigure -f noninteractive tzdata
