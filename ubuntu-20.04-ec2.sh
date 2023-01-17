4ebd8eb3431db91cd1a5132c1802c7f3c4d2d95e
Diego (Azul) Ferreira <30638036+JustAzul@users.noreply.github.com>
2023-01-17 14:09:22 -0300

    First Commit
    first version:
    update everything, set the time to America/Recife, as this is my timezone, and setup swap

---
+++ ubuntu-20.04-ec2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo ln -fs /usr/share/zoneinfo/America/Recife /etc/localtime 
+sudo dpkg-reconfigure -f noninteractive tzdata
+curl https://raw.githubusercontent.com/JustAzul/JustAzul/main/swap.sh | bash
